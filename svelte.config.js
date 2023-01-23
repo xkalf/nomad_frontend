a980f30b540da6df3d86b315e37e00a48a8f52d7
Bilguuntamir <bilguuntamir@qualsmart.com>
2023-01-23 09:54:42 +0800

    a

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/kit/vite'
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,11 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			out: 'build',
-			precompress: false,
-			envPrefix: ''
-		})
+		adapter: adapter()
 	}
 }
 
