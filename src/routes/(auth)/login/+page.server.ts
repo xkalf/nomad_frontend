662cebc7091f8ed4df160591c7fa99d7a99d616c
Bilguuntamir <bilguuntamir@qualsmart.com>
2023-01-27 09:55:13 +0800

    refactoring login server

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,7 +1,7 @@
 import db from '$lib/db'
-import { fail, type Actions, redirect } from '@sveltejs/kit'
+import { fail, redirect } from '@sveltejs/kit'
 import bcrypt from 'bcrypt'
-import type { PageServerLoad } from './$types'
+import type { Action, Actions, PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const ok = locals.user ? true : false
@@ -10,42 +10,42 @@ export const load: PageServerLoad = async ({ locals }) => {
 	}
 }
 
-export const actions: Actions = {
-	login: async ({ request, cookies }) => {
-		const { email, password } = Object.fromEntries(await request.formData()) as {
-			email: string
-			password: string
-		}
-
-		const user = await db.user.findUnique({
-			where: {
-				email
-			}
-		})
-
-		if (!user) {
-			return fail(401, { message: 'Хэрэглэгч олдсонгүй.' })
-		}
-
-		const ok = await bcrypt.compare(password, user.password)
-
-		if (!ok) {
-			return fail(400, { message: 'Нууц үг буруу байна.' })
-		}
-
-		const authUser = await db.user.update({
-			where: { email },
-			data: { token: crypto.randomUUID() }
-		})
-
-		cookies.set('session', authUser.token, {
-			path: '/',
-			httpOnly: true,
-			sameSite: 'strict',
-			secure: process.env.NODE_ENV === 'production',
-			maxAge: 60 * 60 * 24 * 30
-		})
-
-		throw redirect(302, '/')
+const login: Action = async ({ request, cookies }) => {
+	const { email, password } = Object.fromEntries(await request.formData()) as {
+		email: string
+		password: string
 	}
+
+	const user = await db.user.findUnique({
+		where: {
+			email
+		}
+	})
+
+	if (!user) {
+		return fail(401, { message: 'Хэрэглэгч олдсонгүй.' })
+	}
+
+	const ok = await bcrypt.compare(password, user.password)
+
+	if (!ok) {
+		return fail(400, { message: 'Нууц үг буруу байна.' })
+	}
+
+	const authUser = await db.user.update({
+		where: { email },
+		data: { token: crypto.randomUUID() }
+	})
+
+	cookies.set('session', authUser.token, {
+		path: '/',
+		httpOnly: true,
+		sameSite: 'strict',
+		secure: process.env.NODE_ENV === 'production',
+		maxAge: 60 * 60 * 24 * 30
+	})
+
+	throw redirect(302, '/')
 }
+
+export const actions: Actions = { login }
