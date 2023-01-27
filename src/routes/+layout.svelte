d967cf2da0032233750e1c07b12788441dd65289
Bilguuntamir <bilguuntamir@qualsmart.com>
2023-01-27 15:09:53 +0800

    meta image fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import '../app.css'
+	import logo from '$lib/assets/timer-black.png'
 </script>
+
+<svelte:head>
+	<meta name="og:image" content={logo} />
+	<meta name="og:title" content="Нүүдэлчин рубик шооны клуб" />
+	<meta name="og:type" content="website" />
+	<title>Нүүдэлчин клуб</title>
+</svelte:head>
 
 <slot />
