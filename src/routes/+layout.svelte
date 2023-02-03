ee963f4373fc89075a16f0ea355fffde0ede2c6a
Bilguuntamir <bilguuntamir@qualsmart.com>
2023-02-03 15:09:47 +0800

    fix google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
 	import '../app.css'
 	import logo from '$lib/assets/vertical-logo.png'
-	import Analytics from '$lib/analytics.svelte'
+	import { page } from '$app/stores'
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', 'G-5BDWRZD9W5', {
+				page_title: document.title,
+				page_path: $page.url.href
+			})
+		}
+	}
 </script>
 
-<Analytics />
-
 <svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-5BDWRZD9W5">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || []
+
+		function gtag() {
+			dataLayer.push(arguments)
+		}
+
+		gtag('js', new Date())
+		gtag('config', 'G-5BDWRZD9W5')
+	</script>
 	<meta name="og:image" content={logo} />
 	<meta name="og:title" content="Нүүдэлчин рубик шооны клуб" />
 	<meta name="og:type" content="website" />
