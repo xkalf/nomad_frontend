2f9c4fa5353e4cc7b4fc898cb563b64178278a0f
Bilguuntamir <bilguuntamir@qualsmart.com>
2023-02-27 17:15:09 +0800

    fix scramble-display

--- src/lib/components/ScrambleDisplay.svelte
+++ src/lib/components/ScrambleDisplay.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
 	import { cubeType } from '$lib/stores/cubeType'
-	import 'scramble-display'
+	import { onMount } from 'svelte'
 
 	export let scramble: string | null
 	export let mobile = false
+
+	let container: HTMLDivElement
+	let el: any
+
+	onMount(async () => {
+		const { ScrambleDisplay } = await import('scramble-display')
+		el = new ScrambleDisplay()
+		el.event = $cubeType
+		el.visualization = mobile ? '2D' : $cubeType === 'pyram' ? '2D' : '3D'
+		el.style.width = '100%'
+		el.scramble = scramble
+
+		container.appendChild(el)
+	})
+
+	$: if (el) {
+		el.scramble = scramble
+	}
 </script>
 
-<scramble-display
+<div bind:this={container} />
+
+<!-- <scramble-display
 	style="width: 100%;"
 	{scramble}
 	event={$cubeType}
 	visualization={mobile ? '2D' : $cubeType === 'pyram' ? '2D' : '3D'}
-/>
+/> -->
